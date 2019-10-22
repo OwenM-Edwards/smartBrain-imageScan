f2df7d512515ecf0354972662d56dcddde5b8aa5
Quibbly <owenhedwards@gmail.com>
2019-10-22 14:07:25 +0100

    Updates

---
+++ precache-manifest.f4a9aa5068cdc392b5b83402ebda5dfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f35684b4182bc5223ed3b747ac178b3",
+    "url": "/smartBrain-imageScan/index.html"
+  },
+  {
+    "revision": "67e0ba932dfbe14c7438",
+    "url": "/smartBrain-imageScan/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f70ed36f3e0d064f1820",
+    "url": "/smartBrain-imageScan/static/css/main.b841e6fc.chunk.css"
+  },
+  {
+    "revision": "67e0ba932dfbe14c7438",
+    "url": "/smartBrain-imageScan/static/js/2.2666dfb5.chunk.js"
+  },
+  {
+    "revision": "f70ed36f3e0d064f1820",
+    "url": "/smartBrain-imageScan/static/js/main.60e1bddc.chunk.js"
+  },
+  {
+    "revision": "09a7c233176157c6daca",
+    "url": "/smartBrain-imageScan/static/js/runtime-main.d2070db6.js"
+  }
+]);
